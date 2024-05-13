f3d1c12aac7bd8d33650595607f187c408ea5301
James E. King III <jking@apache.org>
2024-05-13 17:56:36 +0000

    Reorganize CI and add support for more containers

--- bin/linux/lib/defaults.sh
+++ bin/linux/lib/defaults.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2018 - 2022 James E. King III
+# Copyright (C) 2018 - 2024 James E. King III
 #
 # Use, modification, and distribution are subject to the
 # Boost Software License, Version 1.0. (See accompanying file
@@ -9,11 +9,20 @@
 # Boost Docker Development Environment
 #
 
+# Defaults
 BDDE_ARCH=${BDDE_ARCH:-x86_64}
 BDDE_DISTRO=${BDDE_DISTRO:-ubuntu}
 BDDE_EDITION=${BDDE_EDITION:-focal}
+BDDE_REGISTRY=${BDDE_REGISTRY:-docker.io/}
 BDDE_REPO=${BDDE_REPO:-jeking3/bdde}
 BDDE_SHELL=${BDDE_SHELL:-/bin/bash}
-BDDE_SLUG=${BDDE_DISTRO}-${BDDE_EDITION}.${BDDE_ARCH}
+BDDE_VERSION=${BDDE_VERSION:-latest}
 
+# The slug is the image tag name
+BDDE_SLUG=${BDDE_DISTRO}-${BDDE_EDITION}-${BDDE_ARCH}-${BDDE_VERSION}
+
+# The full image name: registry, repository, tag
+BDDE_IMAGE=${BDDE_REGISTRY}${BDDE_REPO}:${BDDE_SLUG}
+
+# This allows for INCLUDE+ which is really, really useful!
 export DOCKER_BUILDKIT=1
